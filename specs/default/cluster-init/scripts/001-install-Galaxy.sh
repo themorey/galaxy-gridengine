a451d2cca840e03ed5876647f0e922608a147577
Jerry Morey <themorey@gmail.com>
2021-11-04 08:48:00 -0400

    add Galaxy systemd service
    added a galaxy.service systemd unit
    added logrotate for galaxy.log

--- specs/default/cluster-init/scripts/001-install-Galaxy.sh
+++ specs/default/cluster-init/scripts/001-install-Galaxy.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 set -ex
 
+# Set SELinux to permissive
+setenforce 0 
+sed -i 's/enforcing/permissive' /etc/selinux/config
+
+
+#Install Python3 and Packages
 yum install -y git python3
-python3 -m pip install --upgrade pip
-python3 -m pip install --user virtualenv 
+runuser -l ${sge_user} -c "python3 -m pip install -U --user pip"
+runuser -l ${sge_user} -c "python3 -m pip install --user virtualenv setuptools_rust cloudauthz"
 
 
-# update symbolic link to Python3 
-#ln -sf /bin/python3 /bin/python
+# Set Galaxy install directory to NFS (/shared)
 gal_dir=/shared/Galaxy
+
 
 # Define a cluster user (Cycle Cluster Owner) and start/install Galaxy
 if [ -f /opt/cycle/jetpack/bin/jetpack ]; then
@@ -17,11 +23,12 @@ else
   sge_user=cycleadmin
 fi
 mkdir -p ${gal_dir}
-if $(stat -c '%U' ${gal_dir}) == ${sge_user}; then
+if [[ $(stat -c '%U' ${gal_dir}) == ${sge_user} ]]; then
   echo "skipping...Galaxy dir already owned by ${sge_user}"
 else
   chown -R ${sge_user}:${sge_user} ${gal_dir}
 fi
+
 
 # set local DRMAA environment variable
 if ! grep -qi "DRMAA_LIBRARY_PATH" /etc/profile; then
@@ -42,7 +49,7 @@ chmod 1777 /mnt/resource
 
 
 # CLone the Galaxy repo to shared NFS dir (/shared)
-if [ ! -d ${gal_dir}/run.sh ]; then
+if [ ! -f ${gal_dir}/run.sh ]; then
   runuser -l ${sge_user} -c "git clone https://github.com/galaxyproject/galaxy.git ${gal_dir}"
 fi
 
@@ -58,7 +65,6 @@ if [ ! -f ${gal_dir}/config/auth_conf.xml ]; then
   fi
 fi
 
-
 chown ${sge_user}:${sge_user} ${gal_dir}/config/{galaxy.yml,job_conf.xml,auth_conf.xml}
 
 
@@ -71,10 +77,79 @@ fi
 
 
 # Start Galaxy as a daemon with galaxy.log file
-if [ -d ${CYCLECLOUD_SPEC_PATH} ]; then
-  jetpack log 'Galaxy startup begun'
-  runuser -l ${sge_user} -c "GALAXY_LOG=${gal_dir}/galaxy.log nohup sh ${gal_dir}/run.sh --daemon \
-    > ${gal_dir}/galaxy.log 2>&1; sudo -i jetpack log "Galaxy daemon started"" &
-else
-  runuser -l ${sge_user} -c "GALAXY_LOG=${gal_dir}/galaxy.log sh ${gal_dir}/run.sh --daemon"
-fi
+# if [ -d ${CYCLECLOUD_SPEC_PATH} ]; then
+#  jetpack log 'Galaxy startup begun'
+#  runuser -l ${sge_user} -c "GALAXY_LOG=${gal_dir}/galaxy.log nohup sh ${gal_dir}/run.sh --daemon \
+#    > ${gal_dir}/galaxy.log 2>&1; sudo -i jetpack log 'Galaxy daemon started' " &
+# else
+#  runuser -l ${sge_user} -c "GALAXY_LOG=${gal_dir}/galaxy.log sh ${gal_dir}/run.sh --daemon"
+# fi
+
+
+# Create, enable & start systemd service for Galaxy
+# cat <<EOF >>/etc/systemd/system/galaxy.service
+# [Unit]
+# Description=Galaxy
+# After=network.target
+# After=time-sync.target
+
+# [Service]
+# Environment="SGE_ROOT=$SGE_ROOT"
+# Environment="SGE_CELL=$SGE_CELL"
+# Environment="SGE_EXECD_PORT=$SGE_EXECD_PORT"
+# Environment="SGE_QMASTER_PORT=$SGE_QMASTER_PORT"
+# Environment="SGE_CLUSTER_NAME=$SGE_CLUSTER_NAME"
+# User=${sge_user}
+# WorkingDirectory=${gal_dir}
+# ExecStart=/bin/sh -c 'GALAXY_LOG=${gal_dir}/galaxy.log nohup ${gal_dir}/run.sh --daemon > ${gal_dir}/galaxy.log 2>&1'
+# ExecStop=/bin/bash ${gal_dir}/run.sh --stop-daemon
+# Restart=always
+
+# [Install]
+# WantedBy=multi-user.target reboot.target
+# EOF
+
+cat <<EOF >>/etc/systemd/system/galaxy.service
+[Unit]
+Description=Galaxy
+After=network.target
+After=time-sync.target
+
+[Service]
+Environment="SGE_ROOT=${SGE_ROOT}"
+Environment="SGE_CELL=${SGE_CELL}"
+Environment="SGE_EXECD_PORT=${SGE_EXECD_PORT}"
+Environment="SGE_QMASTER_PORT=${SGE_QMASTER_PORT}"
+Environment="SGE_CLUSTER_NAME=${SGE_CLUSTER_NAME}"
+UMask=022
+Type=simple
+User=$(jetpack config cyclecloud.user.name)
+Group=$(jetpack config cyclecloud.user.name)
+WorkingDirectory=${gal_dir}
+ExecStart=/bin/sh -c '/shared/Galaxy/.venv/bin/uwsgi --yaml /shared/Galaxy/config/galaxy.yml > /shared/Galaxy/galaxy.log 2>&1'
+MemoryLimit=12G
+Restart=always
+
+MemoryAccounting=yes
+CPUAccounting=yes
+BlockIOAccounting=yes
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+systemctl daemon-reload
+systemctl enable galaxy.service
+systemctl start galaxy.service
+
+
+# Create a logrotate file
+cat <<EOF >>/etc/logrotate.d/galaxy
+${gal_dir}/galaxy.log {
+  weekly
+  rotate 8
+  copytruncate
+  compress
+  missingok
+  notifempty
+}
